ddf21872531bee05b4699e25ab85721ff9364866
Sergey Piskunov <iloveooz@yandex.ru>
2019-10-01 15:00:26 +0700

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,5 +1,5 @@
-#include <cstdio>
-#include <cstdlib>
+#include <iostream>
+
 #define SIZE 6
 
 int main() {
@@ -10,66 +10,68 @@ int main() {
 	int temp, minindex, min;
 	int begin_index = 0;
 
-	system("chcp 1251");
+	setlocale(LC_ALL, "Russian");
 
 	// Инициализация матрицы связей
-	for (int i = 0; i<SIZE; i++)
-	{
-	a[i][i] = 0;
-	for (int j = i + 1; j<SIZE; j++) {
-	printf("Введите расстояние %d - %d: ", i + 1, j + 1);
-	scanf("%d", &temp);
-	a[i][j] = temp;
-	a[j][i] = temp;
+	for (int i = 0; i<SIZE; i++) {
+		a[i][i] = 0;
+		for (int j = i + 1; j<SIZE; j++) {
+			std::cout << "Введите расстояние (" << (i + 1) << " - " << (j + 1) << "): ";
+			std::cin >> temp;
+			a[i][j] = temp;
+			a[j][i] = temp;
+		}
 	}
-	}
+
 	// Вывод матрицы связей
-	for (int i = 0; i<SIZE; i++)
-	{
-	for (int j = 0; j<SIZE; j++)
-	printf("%5d ", a[i][j]);
-	printf("\n");
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++)
+				std::cout << a[i][j] << ' ';
+		std::cout << std::endl;
 	}
+
 	//Инициализация вершин и расстояний
-	for (int i = 0; i<SIZE; i++)
-	{
-	d[i] = 10000;
-	v[i] = 1;
+	for (int i = 0; i<SIZE; i++) {
+		d[i] = 10000;
+		v[i] = 1;
 	}
+
 	d[begin_index] = 0;
+
 	// Шаг алгоритма
 	do {
-	minindex = 10000;
-	min = 10000;
-	for (int i = 0; i<SIZE; i++)
-	{ // Если вершину ещё не обошли и вес меньше min
-	if ((v[i] == 1) && (d[i]<min))
-	{ // Переприсваиваем значения
-	min = d[i];
-	minindex = i;
-	}
-	}
-	// Добавляем найденный минимальный вес к текущему весу вершины и сравниваем с текущим минимальным весом вершины
-	if (minindex != 10000)
-	{
-	for (int i = 0; i<SIZE; i++)
-	{
-	if (a[minindex][i] > 0)
-	{
-	temp = min + a[minindex][i];
-	if (temp < d[i])
-	{
-	d[i] = temp;
-	}
-	}
-	}
-	v[minindex] = 0;
-	}
-	} while (minindex < 10000);
+		minindex = 10000;
+		min = 10000;
+
+		for (int i = 0; i<SIZE; i++) {
+			// Если вершину ещё не обошли и вес меньше min
+			if ((v[i] == 1) && (d[i]<min)) { 
+				// Переприсваиваем значения
+				min = d[i];
+				minindex = i;
+			}
+		}
+
+		// Добавляем найденный минимальный вес к текущему весу вершины и сравниваем с текущим минимальным весом вершины
+		if (minindex != 10000) {
+			for (int i = 0; i<SIZE; i++) {
+				if (a[minindex][i] > 0) {
+					temp = min + a[minindex][i];
+					if (temp < d[i]) {
+						d[i] = temp;
+					}
+				}
+			}
+			v[minindex] = 0;
+		}
+	} 
+	while (minindex < 10000);
+
 	// Вывод кратчайших расстояний до вершин
-	printf("\nКратчайшие расстояния до вершин: \n");
+	std::cout << '\n' << "Кратчайшие расстояния до вершин" << '\n';
 	for (int i = 0; i<SIZE; i++)
-	printf("%5d ", d[i]);
+		std::cout << d[i] << ' ';
+	std::cout << std::endl;
 
 	// Восстановление пути
 	int ver[SIZE]; // массив посещенных вершин
@@ -78,25 +80,26 @@ int main() {
 	int k = 1; // индекс предыдущей вершины
 	int weight = d[end]; // вес конечной вершины
 
-	while (end != begin_index) // пока не дошли до начальной вершины
-	{
-	for (int i = 0; i<SIZE; i++) // просматриваем все вершины
-	if (a[end][i] != 0)   // если связь есть
-	{
-	int temp = weight - a[end][i]; // определяем вес пути из предыдущей вершины
-	if (temp == d[i]) // если вес совпал с рассчитанным
-	{                 // значит из этой вершины и был переход
-	weight = temp; // сохраняем новый вес
-	end = i;       // сохраняем предыдущую вершину
-	ver[k] = i + 1; // и записываем ее в массив
-	k++;
+	while (end != begin_index) { // пока не дошли до начальной вершины
+		for (int i = 0; i < SIZE; i++) // просматриваем все вершины
+		if (a[end][i] != 0) { // если связь есть
+			int temp = weight - a[end][i]; // определяем вес пути из предыдущей вершины
+			if (temp == d[i]) { // если вес совпал с рассчитанным, значит из этой вершины и был переход
+				weight = temp; // сохраняем новый вес
+				end = i;       // сохраняем предыдущую вершину
+				ver[k] = i + 1; // и записываем ее в массив
+				k++;
+			}
+		}
 	}
-	}
-	}
+
 	// Вывод пути (начальная вершина оказалась в конце массива из k элементов)
-	printf("\n Вывод кратчайшего пути\n");
+	std::cout << '\n' << "Вывод кратчайшего пути" << '\n';
+
 	for (int i = k - 1; i >= 0; i--)
-	printf("%3d ", ver[i]);
-	getchar(); getchar();
+		std::cout << ver[i] << ' ';
+	std::cout << std::endl;
+	
+	system("pause");
 	return 0;
 }
