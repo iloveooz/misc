6e99bdd011b50e930e459bedfd95241bb454ac43
Sergey Piskunov <iloveooz@yandex.ru>
2019-11-05 18:21:41 +0700

    Update blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -43,3 +43,17 @@ int Card::GetValue() const {
 void Card::Flip() {
 	m_IsFaceUp = !(m_IsFaceUp);
 }
+
+class Hand {
+public:
+	Hand();
+	virtual ~Hand();
+	// добавляет карту в руку
+	void Add(Card* pCard);
+	// очищает руку от карт
+	void Clear();
+	// получает сумму очков карт в руке, присваивая тузу значение 1 или 11 в зависимости от ситуации
+	int GetTotal() const;
+protected:
+	std::vector<Card*> m_Cards;
+};
