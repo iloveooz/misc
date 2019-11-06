baa2fff7629fbfaaee95a1f6436d4f55ee0c6031
Sergey Piskunov <iloveooz@yandex.ru>
2019-11-06 13:10:55 +0700

    Update blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -126,11 +126,9 @@ protected:
 	std::string m_Name;
 };
 
-GenericPlayer::GenericPlayer(const std::string& name): m_Name(name)
-{}
+GenericPlayer::GenericPlayer(const std::string& name): m_Name(name) {}
 
-GenericPlayer::~GenericPlayer()
-{}
+GenericPlayer::~GenericPlayer() {}
 
 bool GenericPlayer::IsBusted() const {
 return (GetTotal() > 21);
@@ -154,11 +152,9 @@ public:
 	void Push() const;
 };
 
-Player::Player (const std::string& name) : GenericPlayer(name)
-{}
+Player::Player (const std::string& name) : GenericPlayer(name) {}
 
-Player::~Player()
-{}
+Player::~Player() {}
 
 bool Player::IsHitting() const {
 	std::cout << m_Name << ". Do you want a hit? (Y/N): ";
@@ -189,11 +185,9 @@ public:
 	void FlipFirstCard();
 };
 
-House::House(const std::string& name) : GenericPlayer(name)
-{}
+House::House(const std::string& name) : GenericPlayer(name) {}
 
-House::~House()
-{}
+House::~House() {}
 
 bool House::IsHitting() const {
 return (GetTotal() <= 16);
@@ -206,4 +200,206 @@ void House::FlipFirstCard() {
 	else {
 		std::cout << "No card to flip!\n";
 	}
+}
+
+class Deck : public Hand {
+public:
+	Deck();
+	virtual ~Deck();
+	// создает стандартную колоду из 52 карт
+	void Populate();
+	// тасует карты
+	void Shuffle();
+	// раздает одну карту в руку
+	void Deal(Hand& aHand);
+	// дает дополнительные карты игроку
+	void AdditionalCards(GenericPlayer& aGenericPlayer);
+};
+
+Deck::Deck() {
+	m_Cards.reserve(52);
+	Populate();
+}
+
+Deck::~Deck() {}
+
+void Deck::Populate() {
+	Clear();
+	// создает стандартную колоду
+	for (int s = Card::CLUBS; s <= Card::SPADES; ++s) {
+		for (int r = Card::АСЕ; r <= Card::KING; ++r) {
+			Add(new Card(static_cast<Card::rank>(r), static_cast<Card::suit>(s)));
+		}
+	}
+}
+
+void Deck::Shuffle() {
+	std::random_shuffle(m_Cards.begin(), m_Cards.end());
+}
+
+void Deck::Deal(Hand& aHand) {
+	if (!m_Cards.empty()) {
+		aHand.Add(m_Cards.back());
+		m_Cards.pop_back();
+	}
+	else {
+		std::cout << "Out of cards. Unable to deal.";
+	}
+}
+
+void Deck::AdditionalCards(GenericPlayer& aGenericPlayer) {
+	std::cout << std::endl;
+	// продолжает раздавать карты до тех пор, пока у игрока не случается перебор или пока он хочет взять еще одну карту
+	while (!(aGenericPlayer.IsBusted()) && aGenericPlayer.IsHitting()) {
+		Deal(aGenericPlayer);
+		std::cout << aGenericPlayer << std::endl;
+		if (aGenericPlayer.IsBusted()) {
+			aGenericPlayer.Bust();
+		}
+	}
+}
+
+class Game {
+public:
+	Game(const std::vector<std::string>& names);
+	~Game();
+	// проводит игру в Blackjack
+	void Plау();
+private:
+	Deck m_Deck;
+	House m_House;
+	std::vector<Player> m_Players;
+};
+
+Game::Game(const std::vector<std::string>& names) {
+	// создает вектор игроков из вектора с именами
+	std::vector<std::string>::const_iterator pName;
+	for (pName = names.begin(); pName != names.end(); ++pName) {
+		m_Players.push_back(Player(*pName));
+	}
+	// засевает генератор случайных чисел
+	srand(static_cast<unsigned int>(time(0)));
+	m_Deck.Populate();
+	m_Deck.Shuffle();
+}
+
+Game::~Game() {}
+
+void Game::Plау() {
+	// раздает каждому по две стартовые карты
+	std::vector<Player>::iterator pPlayer;
+	for (int i = 0; i < 2; ++i) {
+		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
+			m_Deck.Deal(*pPlayer);
+		}
+		m_Deck.Deal(m_House);
+	}
+	// прячет первую карту дилера
+	m_House.FlipFirstCard();
+	// открывает руки всех игроков
+	for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
+			std::cout << *pPlayer << std::endl;
+	}
+	std::cout << m_House << std::endl;
+	// раздает игрокам дополнительные карты
+	for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
+		m_Deck.AdditionalCards(*pPlayer);
+	}
+	// показывает первую карту дилера
+	m_House.FlipFirstCard();
+	std::cout << std::endl << m_House;
+	// раздает дилеру дополнительные карты
+	m_Deck.AdditionalCards(m_House);
+	if (m_House.IsBusted()) {
+		// все, кто остался в игре, побеждают
+		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
+			if (!pPlayer->IsBusted()) {
+				pPlayer->Win();
+			}
+		}
+	}
+	else {
+		// сравнивает суммы очков всех оставшихся игроков с суммой очков дилера
+		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
+			if (!pPlayer->IsBusted()) {
+				if (pPlayer->GetTotal() > m_House.GetTotal()) {
+					pPlayer->Win();
+				}
+				else if (pPlayer->GetTotal() < m_House.GetTotal()) {
+					pPlayer->Lose();
+				}
+				else {
+					pPlayer->Push();
+				}
+			}
+		}
+	}
+	// очищает руки всех игроков
+	for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
+		pPlayer->Clear();
+	}
+	m_House.Clear();
+}
+
+
+// прототипы функций
+std::ostream& operator<<(std::ostream& os, const Card& aCard);
+std::ostream& operator<<(std::ostream& os, const GenericPlayer& aGenericPlayer);
+
+int main() {
+	std::cout << "\t\tWelcome to Blackjack!\n\n";
+	int numPlayers = 0;
+	while (numPlayers < 1 || numPlayers > 7) {
+		std::cout << "How many players? С1 - 7): ";
+		std::cin >> numPlayers;
+	}
+	std::vector<std::string> names;
+	std::string name;
+	for (int i = 0; i < numPlayers; ++i) {
+		std::cout << "Enter player name: ";
+		std::cin >> name;
+		names.push_back(name);
+	}
+	std::cout << std::endl;
+	// игровой цикл
+	Game aGame(names);
+	char again = 'y';
+	while (again != 'n' && again != 'N') {
+		aGame.Plау();
+		std::cout << "\nDo you want to play again? (Y/N): ";
+		std::cin >> again;
+	}
+	return 0;
+}
+
+	
+// перегружает оператор <<, чтобы получить возможность отправить объект типа Card в поток std::cout
+std::ostream& operator<<(std::ostream& os, const Card& aCard) {
+	const std::string RANKS[] ={"0", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	const std::string SUITS[] ={"c", "d", "h", "s"};
+	if (aCard.m_IsFaceUp) {
+		os << RANKS[aCard.m_Rank] << SUITS[aCard.m_Suit];
+	}
+	else {
+		os << "XX";
+	}
+	return os;
+}
+
+// перегружает оператор <<, чтобы получить возможность отправить объект типа GenericPlayer в поток std::cout
+std::ostream& operator<<(std::ostream& os, const GenericPlayer& aGenericPlayer) {
+	os << aGenericPlayer.m_Name << ":\t";
+	std::vector<Card *>::const_iterator pCard;
+	if (!aGenericPlayer.m_Cards.empty()) {
+		for (pCard = aGenericPlayer.m_Cards.begin(); pCard != aGenericPlayer.m_Cards.end();	++pCard) {
+			os << *(*pCard) << "\t";
+		}
+		if (aGenericPlayer.GetTotal() != 0) {
+			std::cout << "(" << aGenericPlayer.GetTotal() << ")";
+		}
+	}
+	else {
+		os << "<empty>";
+	}
+	return os;
 }
