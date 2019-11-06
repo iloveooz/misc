f7852b5523ef3a18266ceced6ba9a87cb7a36643
Sergey Piskunov <iloveooz@yandex.ru>
2019-11-06 13:46:48 +0700

    Update blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -314,7 +314,7 @@ void Game::Plау() {
 	if (m_House.IsBusted()) {
 		// все, кто остался в игре, побеждают
 		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
-			if (!pPlayer->IsBusted()) {
+			if (!(pPlayer->IsBusted())) {
 				pPlayer->Win();
 			}
 		}
@@ -322,7 +322,7 @@ void Game::Plау() {
 	else {
 		// сравнивает суммы очков всех оставшихся игроков с суммой очков дилера
 		for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer) {
-			if (!pPlayer->IsBusted()) {
+			if (!(pPlayer->IsBusted())) {
 				if (pPlayer->GetTotal() > m_House.GetTotal()) {
 					pPlayer->Win();
 				}
@@ -348,16 +348,17 @@ std::ostream& operator<<(std::ostream& os, const Card& aCard);
 std::ostream& operator<<(std::ostream& os, const GenericPlayer& aGenericPlayer);
 
 int main() {
-	std::cout << "\t\tWelcome to Blackjack!\n\n";
+	setlocale(LC_ALL, "Russian");
+	std::cout << "\t\Добро пожаловать в Blackjack!\n\n";
 	int numPlayers = 0;
 	while (numPlayers < 1 || numPlayers > 7) {
-		std::cout << "How many players? (1 - 7): ";
+		std::cout << "Введите количество игроков? (1 - 7): ";
 		std::cin >> numPlayers;
 	}
 	std::vector<std::string> names;
 	std::string name;
 	for (int i = 0; i < numPlayers; ++i) {
-		std::cout << "Enter player name: ";
+		std::cout << "Введите имя игрока: ";
 		std::cin >> name;
 		names.push_back(name);
 	}
@@ -367,17 +368,18 @@ int main() {
 	char again = 'y';
 	while (again != 'n' && again != 'N') {
 		aGame.Plау();
-		std::cout << "\nDo you want to play again? (Y/N): ";
+		std::cout << "\nЕщё партеечку? (Y/N): ";
 		std::cin >> again;
 	}
+	system("pause");
 	return 0;
 }
 
 	
 // перегружает оператор <<, чтобы получить возможность отправить объект типа Card в поток std::cout
 std::ostream& operator << (std::ostream& os, const Card& aCard) {
-	const std::string RANKS[] = { "0", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
-	const std::string SUITS[] = { "c", "d", "h", "s" };
+	const std::string RANKS[] = { "0", "Т", "2", "3", "4", "5", "6", "7", "8", "9", "10", "В", "Д", "К" };
+	const std::string SUITS[] = { "к", "б", "ч", "п" };
 	if (aCard.m_IsFaceUp) {
 		os << RANKS[aCard.m_Rank] << SUITS[aCard.m_Suit];
 	}
