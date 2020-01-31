6858447cd20f6d5a70d27a60849bff7c4101d06b
Sergey Piskunov <iloveooz@yandex.ru>
2020-01-31 18:58:53 +0700

    Create 2109.cpp

---
+++ 2109.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a, b, c, d, e;
+  cin >> a >> b >> c >> d >> e;
+
+    std::cout << e + (e - d) + 1 << ' ' << e + (e - d) + (e - d) + 3 << ' ' << e + (e - d) + (e - d) + (e - d) + 6 << std::endl;
+    
+  return 0;   
+ }
